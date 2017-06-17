acb55ed46cb854be17ac33bc24732c5c05a6f113
Philipp <philipp.bode@student.hpi.de>
2017-06-17 22:26:45 +0200

    Add sqlalchemy database connection

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    from sqlalchemy import text
+
+    sql = text("""SELECT "VALUE" FROM "HPI_2017"."DATAPOINTS" WHERE "KEY" = 'CALORIES' LIMIT 10""")
+    result = db.engine.execute(sql)
+    names = []
+    for row in result:
+        names.append(row[0])
+    return str(names)
