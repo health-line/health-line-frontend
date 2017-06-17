36abe5ff040a42e9314595f00a4b198954ae8fdd
Christian Friedow <christian@friedow.com>
2017-06-17 20:12:32 +0200

    Added react router

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import Dashboard from 'Dashboard/Dashboard';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <MuiThemeProvider>
-	            <AppBar
-		            title="HackHPI 2017"
-		            iconClassNameRight="muidocs-icon-navigation-expand-more"
-	            />
-
+	            <div>
+		            <AppBar
+			            title="HackHPI 2017"
+			            iconClassNameRight="muidocs-icon-navigation-expand-more"
+		            />
+		            <Router>
+			            <Route exact path="/" component={Dashboard}/>
+		            </Router>
+	            </div>
             </MuiThemeProvider>
         );
     }
