1a750ab0e6e0e710895effb9cc3fb3c7a4df36d0
Christian Friedow <christian@friedow.com>
2017-06-17 19:58:04 +0200

    Changed index to match moved files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// Material UI dependency
+injectTapEventPlugin();
+// React dependency
 registerServiceWorker();
