62c17b04fe52fdacf568e815d391ed5e82f530bb
Christian Friedow <christian@friedow.com>
2017-06-18 09:09:19 +0200

    Refactored User display

--- frontend/src/App/Dashboard/User/User.js
+++ frontend/src/App/Dashboard/User/User.js
@@ -7,28 +7,56 @@ import FavoriteIcon from 'material-ui/svg-icons/action/favorite';
 import AccessibilityIcon from 'material-ui/svg-icons/action/accessibility';
 import WCIcon from 'material-ui/svg-icons/notification/wc';
 import settings from '../../../settings';
-import './../Dashboard.css';
+import './User.css';
 
 class User extends Component {
 
+	getFormattedBirth(birthString) {
+		const monthNames = [
+			"January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"
+		];
+		const birth = new Date(birthString);
+		return birth.getDate() + ". "+ monthNames[birth.getMonth()] + " " + birth.getFullYear();
+	}
+
+	getFormattedHeight(height) {
+		const heightInMeters = height / 100;
+		return heightInMeters + ' Meters'
+	}
+
 	render() {
         const { user } = this.props;
-
         if (user.pending) {
             return <div>Loading...</div>
         } else if (user.rejected) {
 			return <span>{user.reason}</span>
         } else if (user.fulfilled) {
         	const userData = user.value;
+			const formattedBirth = this.getFormattedBirth(userData["BIRTHDAY"]);
+	        const formattedHeight = this.getFormattedHeight(userData["HEIGHT"]);
         	return (
 				<div className="col-xs-12 col-sm-6">
 					<Card className="h100">
 						<CardText>
 							<List>
-								<ListItem primaryText={userData["NAME"]} leftIcon={<PersonIcon />}/>
-								<ListItem primaryText={userData["BIRTHDAY"]} leftIcon={<FavoriteIcon />}/>
-								<ListItem primaryText={userData["SEX"]} leftIcon={<WCIcon />}/>
-								<ListItem primaryText={userData["HEIGHT"]} leftIcon={<AccessibilityIcon />}/>
+								<ListItem
+									primaryText={userData["NAME"]}
+									secondaryText="Name"
+									leftIcon={<PersonIcon />}/>
+								<ListItem
+									primaryText={formattedBirth}
+									secondaryText="Birthday"
+									leftIcon={<FavoriteIcon />}/>
+								<ListItem
+									className="gender"
+									primaryText={<span>{userData["SEX"]}</span>}
+									secondaryText="Gender"
+									leftIcon={<WCIcon />}/>
+								<ListItem
+									primaryText={formattedHeight}
+									secondaryText="Body Height"
+									leftIcon={<AccessibilityIcon />}/>
 							</List>
 						</CardText>
 					</Card>
