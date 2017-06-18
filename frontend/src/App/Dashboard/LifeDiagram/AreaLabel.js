42769774f9d8e197eb4720a1b202f914cb98f95c
MaximilianV <maximilian.voelker@mailbox.org>
2017-06-18 10:00:32 +0200

    fix label on diagram

---
+++ frontend/src/App/Dashboard/LifeDiagram/AreaLabel.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import "./../Dashboard.css";
+
+class AreaLabel extends Component {
+
+
+    render() {
+        return(
+            <a href="https://developer.mozilla.org/en-US/docs/SVG"
+               target="_blank">
+                <text fill="black" textAnchor="middle"
+                      y="21" x="60">{this.props.label}</text>
+            </a>
+        )
+    }
+}
+
+
+export default AreaLabel;
--- frontend/src/App/Dashboard/LifeDiagram/LifeDiagram.js
+++ frontend/src/App/Dashboard/LifeDiagram/LifeDiagram.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
-import {Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis, ReferenceArea} from "recharts";
 import "./../Dashboard.css";
-import {connect} from "react-refetch";
+import {connect, PromiseState} from "react-refetch";
 import settings from "../../../settings";
+import AreaLabel from "./AreaLabel";
 import data from "../../../data/mocked_data.json";
 
 class LifeDiagram extends Component {
@@ -11,44 +12,63 @@ class LifeDiagram extends Component {
 		this.data = data;
 	}
 
-	componentDidMount() {
-		document.addEventListener("DOMContentLoaded", function() {
-			Array.from(document.getElementsByClassName("recharts-reference-area-rect")).forEach(item => {
-				if(item.nextSibling) {
-					const x = parseFloat(item.getAttribute("x"));
-					const width = parseFloat(item.getAttribute("width")) / 2;
-					item.nextSibling.firstChild.setAttribute("x", width + x);
-				}
-			});
-		});
+	componentDidUpdate() {
+        setTimeout(function(){
+            Array.from(document.getElementsByClassName("recharts-reference-area-rect")).forEach(item => {
+                if(item.nextSibling) {
+                    const x = parseFloat(item.getAttribute("x"));
+                    const y = parseFloat(item.getAttribute("y"));
+                    const width = parseFloat(item.getAttribute("width")) / 2;
+                    const height = parseFloat(item.getAttribute("height") / 3);
+                    item.nextSibling.firstChild.setAttribute("x", width + x);
+                    item.nextSibling.firstChild.setAttribute("y", height + y);
+                }
+            });
+		}, 3000);
 	}
 
 	render() {
-        const { data } = this.props;
+        const { data, events } = this.props;
 
-        if (data.pending) {
+        const allFetches = PromiseState.all([data, events])
+
+        if (allFetches.pending) {
             return <div>Loading...</div>;
-        } else if (data.rejected) {
-            return <span>{data.reason}</span>;
-        } else if (data.fulfilled) {
-        	console.log(this.props.selectedKeys);
+        } else if (allFetches.rejected) {
+            return <span>{allFetches.reason}</span>;
+        } else if (allFetches.fulfilled) {
             return (
 				<ResponsiveContainer height={300}>
 					<AreaChart
 						data={data.value}
 						margin={{top: 20, right: 30, left: 0, bottom: 0}}>
-						<XAxis dataKey="DATE"/>
+						<XAxis xAxisId={0} dataKey="DATE"/>
 						<CartesianGrid strokeDasharray="3 3"/>
 						<Tooltip />
                         {this.props.selectedKeys.map((datakey, index) => {
-                            return (<YAxis yAxisId={datakey} hide={true} key={index}/>);
+                            return (<YAxis yAxisId={datakey} hide={true} key={`yaxis`+index.toString()}/>);
                         })}
                         {this.props.selectedKeys.map((datakey, index) => {
                             return (
 								<Area type="monotone" dataKey={datakey} yAxisId={datakey} stroke="#8884d8"
-									  fill="#8884d8" key={index}/>
+									  fill="#8884d8" key={`area`+index.toString()}/>
                             );
                         })}
+						{events.value.map((dataEvent, index) => {
+							const startDateString = new Date(dataEvent["DATE_START"]).toISOString().split("T")[0];
+                            const endDateString = new Date(dataEvent["DATE_END"]).toISOString().split("T")[0];
+							return (
+								<ReferenceArea
+									xAxisId={0}
+									yAxisId={this.props.selectedKeys[0]}
+									x1={startDateString}
+									x2={endDateString}
+									label={<AreaLabel label={dataEvent["TITLE"]} />}
+									key={`refArea`+index.toString()}
+									stroke="red"
+									strokeOpacity={0.3} />
+							);
+						})}
 					</AreaChart>
 				</ResponsiveContainer>
             );
@@ -58,5 +78,6 @@ class LifeDiagram extends Component {
 
 
 export default connect(props => ({
+	events: settings.backendUrl + `/user/${props.userId}/events/`,
 	data: settings.backendUrl + `/user/${props.userId}/data/${props.selectedKeys.join("+")}/start/${props.startDate}/end/${props.endDate}`
 }))(LifeDiagram)
