fe4b11fd5a390f52948274b5729ec77f3953178a
Christian Friedow <christian@friedow.com>
2017-06-18 10:13:13 +0200

    Added EventTable functionality

--- frontend/src/App/Dashboard/EventTable/EventTable.js
+++ frontend/src/App/Dashboard/EventTable/EventTable.js
@@ -6,6 +6,71 @@ import settings from '../../../settings';
 
 class EventTable extends Component {
 
+	constructor() {
+		super();
+		this.state = {
+			displayDetails: false,
+			displayedEvent: 0
+		};
+		this.onEventClick = this.onEventClick.bind(this);
+	}
+
+	onEventClick(index) {
+		this.setState({
+			displayDetails: true,
+			displayedEvent: index
+		});
+	}
+
+	getFormattedDate(dateString) {
+		const monthNames = [
+			"January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"
+		];
+		const date = new Date(dateString);
+		return date.getDate() + ". "+ monthNames[date.getMonth()] + " " + date.getFullYear();
+	}
+
+	displayAttributes(eventData) {
+		if(this.state.displayDetails) {
+			console.log(this.state.displayedEvent);
+			const currentEvent = eventData[this.state.displayedEvent];
+
+			return(
+				<List>
+					<ListItem
+						primaryText={currentEvent["TITLE"]}
+						secondaryText="Title"/>
+					<ListItem
+						primaryText={currentEvent["DESCRIPTION"]}
+						secondaryText="Description"/>
+					<ListItem
+						primaryText={this.getFormattedDate(currentEvent["DATE_START"])}
+						secondaryText="Start Date"/>
+					<ListItem
+						primaryText={this.getFormattedDate(currentEvent["DATE_END"])}
+						secondaryText="End Date"/>
+				</List>
+			);
+		}
+		else {
+			return(
+				eventData.map((event, index) => {
+					return(
+						<ListItem
+							onClick={() => this.onEventClick(index)}
+							primaryText={event["TITLE"]}
+							secondaryText={event["DESCRIPTION"]}/>
+					);
+				})
+			);
+		}
+	}
+
+	onDisplayEventDetails() {
+
+	}
+
 	render() {
 		const { events } = this.props;
 		if (events.pending) {
@@ -19,13 +84,7 @@ class EventTable extends Component {
 					<Card className="h100">
 						<CardText>
 							<List>
-								{eventData.map((event) => {
-									return(
-										<ListItem
-											primaryText={event["TITLE"]}
-											secondaryText={event["DESCRIPTION"]}/>
-									);
-								})}
+								{ this.displayAttributes(eventData) }
 							</List>
 						</CardText>
 					</Card>
